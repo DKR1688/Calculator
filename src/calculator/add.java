406e287cca2e00fb71f69bd97221f152dda3b3fa
Abhayrajput-cs <abhay.singh5_cs22@gla.ac.in>
2026-01-14 15:06:00 +0530

    conflict resolved2

--- src/calculator/add.java
+++ src/calculator/add.java
@@ -4,5 +4,9 @@ public class add {
 	public static void main(String[] args) {
 		System.out.println("hululululu");
 		System.out.println("hahahahahahah");
+		int a=10;
+		int b=1199;
+		int c=a+b;
+		System.out.println(c);
 	}
 }
